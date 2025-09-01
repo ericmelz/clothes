df7e9a8d0c01d557b09e2f9f089072b6b04e26e1
ericmelz <eric@emelz.com>
2025-09-01 14:54:17 -0700

    update deploy.sh to not use website path

--- claude/deploy.sh
+++ claude/deploy.sh
@@ -1,7 +1,7 @@
 set -x
 
 # Copy site to nginx-data repo
-rsync -avz --delete output/website/ $HOME/Data/code/nginx-data/www/ericmelz.site
+rsync -avz --delete output/ $HOME/Data/code/nginx-data/www/ericmelz.site
 
 # Push site using github actions
 CWD=$PWD
